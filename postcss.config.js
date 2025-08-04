9dd09d25e15028c0b2b36cb783d7df2cbc97b765
Yakubu Binuyaminu <binuyaminu>
2025-08-04 15:19:39 +0100

    Configure PostCSS for Tailwind CSS plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: {
-        tailwindcss: {}, // This tells PostCSS to use the Tailwind CSS plugin
-        autoprefixer: {}, // This ensures CSS prefixes are added for browser compatibility
+        tailwindcss: {},
+        autoprefixer: {},
     },
 };
